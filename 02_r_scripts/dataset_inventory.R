36cc4ca298b39687d904fc9d87eff9e7d9cd0024
Wojtek <klimekw44@gmail.com>
2026-01-07 20:47:00 +0100

    refactor `dataset_inventory.R`: - change how folder is displayed to improve readibility

--- 02_r_scripts/dataset_inventory.R
+++ 02_r_scripts/dataset_inventory.R
@@ -8,7 +8,7 @@ library(hms)
 #fitbit_structure_tree <- dir_tree(path = "00_raw_data/MyFitbitData", type = "directory")
 fitbit_files <- dir_ls(path = "00_raw_data/MyFitbitData", recurse = TRUE)
 fitbit_inventory <- tibble(path = fitbit_files,
-                           folder = path_dir(fitbit_files),
+                           folder = path_file(path_dir(fitbit_files)),#extract last element of the file's directory (i.e. folder)
                            file = path_file(fitbit_files),
                            ext = path_ext(fitbit_files),
                            size_kb = file_info(fitbit_files)$size
